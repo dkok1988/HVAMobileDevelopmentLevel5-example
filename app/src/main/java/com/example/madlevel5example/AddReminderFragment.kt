6345026ba8d3e7f09273a78158465f201f6242b0
Dave Kok <dave.kok@nn.nl>
2020-11-28 15:06:26 +0100

    added new logic

---
+++ app/src/main/java/com/example/madlevel5example/AddReminderFragment.kt
@@ -0,0 +1,54 @@
+package com.example.madlevel5example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.madlevel2example.Reminder
+import kotlinx.android.synthetic.main.fragment_add_reminder.*
+
+class AddReminderFragment : Fragment() {
+
+    private val viewModel: ReminderViewModel by viewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_reminder, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnAddReminder.setOnClickListener {
+            onAddReminder()
+        }
+    }
+
+    private fun onAddReminder() {
+        val reminderText = etReminderName.text.toString()
+
+        if (reminderText.isNotBlank()) {
+            //set the data as fragmentResult, we are listening for REQ_REMINDER_KEY in RemindersFragment!
+//            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+            viewModel.insertReminder(Reminder(reminderText))
+
+
+            //"pop" the backstack, this means we destroy
+            //this fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+
+        } else {
+            Toast.makeText(
+                activity,
+                R.string.not_valid_reminder, Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/madlevel5example/RemindersFragment.kt
@@ -0,0 +1,80 @@
+package com.example.madlevel5example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2example.Reminder
+import kotlinx.android.synthetic.main.fragment_reminders.*
+
+class RemindersFragment : Fragment() {
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+
+    private val viewModel: ReminderViewModel by viewModels()
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_reminders, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        observeAddReminderResult()
+    }
+
+    private fun initViews() {
+        rvReminders.layoutManager =
+                LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rvReminders.adapter = reminderAdapter
+        rvReminders.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
+    }
+
+    private fun observeAddReminderResult() {
+        viewModel.reminders.observe(viewLifecycleOwner, Observer { reminders ->
+            this@RemindersFragment.reminders.clear()
+            this@RemindersFragment.reminders.addAll(reminders)
+            reminderAdapter.notifyDataSetChanged()
+        })
+    }
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                val reminderToDelete = reminders[position]
+
+                viewModel.deleteReminder(reminderToDelete)
+
+
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+}
